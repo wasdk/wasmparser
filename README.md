6a7dc38acb35571ca9aeceddd0ee21d1be2dbe0f
Yury Delendik <ydelendik@mozilla.com>
2016-11-01 17:47:44 -0500

    npm fixes.

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple streamable WebAssembly binary parser.
--- disassemble-wasm.js
+++ disassemble-wasm.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 /* Copyright 2016 Mozilla Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
