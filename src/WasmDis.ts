64a7f776e896e3c5aea8371456d84d0052b577be
Benedikt Meurer <bmeurer@google.com>
2020-06-23 08:30:15 +0200

    fix: imports need to use .js suffix to work correctly
    This reverts commit 8abb34b59ec0820259587db07cac95a94a54958e,
    which changed the import of "WasmParser.js" in "WasmDis.ts",
    and that broke the ESM export.

--- src/WasmDis.ts
+++ src/WasmDis.ts
@@ -46,7 +46,7 @@ import {
   NULL_FUNCTION_INDEX,
   isTypeIndex,
   ILocalNameEntry,
-} from "./WasmParser";
+} from "./WasmParser.js";
 
 const NAME_SECTION_NAME = "name";
 const INVALID_NAME_SYMBOLS_REGEX = /[^0-9A-Za-z!#$%&'*+.:<=>?@^_`|~\/\-]/;
